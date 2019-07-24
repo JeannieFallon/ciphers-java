06996ecffddd57542f7da271a3345caf6a626f8d
JeannieFallon <jeanniefallon@gmail.com>
2019-07-24 07:00:02 -0400

    spring boot base run

--- src/main/java/com/metatarsal/ciphersjava/CiphersJavaApplication.java
+++ src/main/java/com/metatarsal/ciphersjava/CiphersJavaApplication.java
@@ -1,13 +1,18 @@
 package com.metatarsal.ciphersjava;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CiphersJavaApplication {
+public class CiphersJavaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiphersJavaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		System.out.println("Hello, Clarice.");
+	}
 }
