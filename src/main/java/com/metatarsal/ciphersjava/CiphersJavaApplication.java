5d32a43ff1f9e86fe7b350d7e1e2896e4ab3aa7a
JeannieFallon <jeanniefallon@gmail.com>
2019-07-24 06:46:16 -0400

    spring boot starter

---
+++ src/main/java/com/metatarsal/ciphersjava/CiphersJavaApplication.java
@@ -0,0 +1,13 @@
+package com.metatarsal.ciphersjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiphersJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiphersJavaApplication.class, args);
+	}
+
+}
