5fe2649b4f4642713a8b0f7f080db6a82bba3df2
JeannieFallon <jeanniefallon@gmail.com>
2019-07-25 07:30:34 -0400

    remove redundant lombok param

--- src/main/java/com/metatarsal/ciphers/models/Rot13Text.java
+++ src/main/java/com/metatarsal/ciphers/models/Rot13Text.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode()
 public class Rot13Text extends Text {
 
     private static final int KEY = 13;
